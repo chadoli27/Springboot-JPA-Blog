db01fffc34044e648462105b182acf2e75768283
chadoli27 <jasminekim1009@gmail.com>
2025-04-09 14:17:32 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,15 @@
+package com.cos.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class BlogControllerTest {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello springboot</h1>";
+	}
+	
+}
